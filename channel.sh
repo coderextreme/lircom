9c2debbda9fa3f4adfa3735ed0e9c757d6733bd6
John Carlson <yottzumm@gmail.com>
2020-04-18 06:17:58 -0500

    added more shell scripts

---
+++ channel.sh
@@ -0,0 +1,4 @@
+export DIR=`pwd`
+export CLASSPATH="$DIR/target/lircom-1.jar:$DIR/m2/httpunit-1.7.3.jar:$DIR/m2/log4j-1.2.17.jar:$DIR/m2/nekohtml-1.9.22.jar:$DIR/m2/xercesImpl-2.11.0.jar:$DIR/m2/rhino-1.7R4.jar:$DIR/m2/json-20190722.jar"
+
+java -Xmx256m -classpath $CLASSPATH lircom.MainWindow lircom.Channel
