6b87b3c471a0f33b37aff42cb853d474a8d4a8ca
John Carlson <coderextreme@coderextreme-Kubuntu20.10>
2021-01-18 21:01:42 -0600

    First run works

--- impact3d.sh
+++ impact3d.sh
@@ -1,9 +1,8 @@
 #!/bin/bash -x
-# make peer &
-# sleep 1
+make peer &
+sleep 1
 export DIR=`pwd`
-export JOGAMP_JARS="${DIR}/m2"
-export JOGAMP_NATIVES="${DIR}/natives/linux-amd64"
+export JARS="${DIR}/m2"
 
 
-java -Xmx256m -cp "${DIR}/target/lircom-1.jar:${JOGAMP_JARS}/gluegen-rt.jar:${JOGAMP_JARS}/gluegen-rt-natives-android-aarch64.jar:${JOGAMP_JARS}/gluegen-rt-natives-android-armv6.jar:${JOGAMP_JARS}/gluegen-rt-natives-linux-amd64.jar:${JOGAMP_JARS}/gluegen-rt-natives-linux-armv6hf.jar:${JOGAMP_JARS}/gluegen-rt-natives-linux-armv6.jar:${JOGAMP_JARS}/gluegen-rt-natives-linux-i586.jar:${JOGAMP_JARS}/gluegen-rt-natives-macosx-universal.jar:${JOGAMP_JARS}/gluegen-rt-natives-solaris-amd64.jar:${JOGAMP_JARS}/gluegen-rt-natives-solaris-i586.jar:${JOGAMP_JARS}/gluegen-rt-natives-windows-amd64.jar:${JOGAMP_JARS}/gluegen-rt-natives-windows-i586.jar:${JOGAMP_JARS}/jogl-all.jar:${JOGAMP_JARS}/jogl-all-natives-android-aarch64.jar:${JOGAMP_JARS}/jogl-all-natives-android-armv6.jar:${JOGAMP_JARS}/jogl-all-natives-linux-amd64.jar:${JOGAMP_JARS}/jogl-all-natives-linux-armv6hf.jar:${JOGAMP_JARS}/jogl-all-natives-linux-armv6.jar:${JOGAMP_JARS}/jogl-all-natives-linux-i586.jar:${JOGAMP_JARS}/jogl-all-natives-macosx-universal.jar:${JOGAMP_JARS}/jogl-all-natives-solaris-amd64.jar:${JOGAMP_JARS}/jogl-all-natives-solaris-i586.jar:${JOGAMP_JARS}/jogl-all-natives-windows-amd64.jar:${JOGAMP_JARS}/jogl-all-natives-windows-i586.jar" impact.Impact3D
+java -Xmx256m -cp "${DIR}/target/lircom-1.jar:${JARS}/gluegen-rt-android-natives-linux-amd64.jar:${JARS}/gluegen-rt.jar:${JARS}/gluegen-rt-natives-linux-amd64.jar:${JARS}/httpunit-1.7.3.jar:${JARS}/joal-android-natives-linux-amd64.jar:${JARS}/joal-natives-linux-amd64.jar:${JARS}/jocl-android-natives-linux-amd64.jar:${JARS}/jocl-natives-linux-amd64.jar:${JARS}/jogl-all-android-natives-linux-amd64.jar:${JARS}/jogl-all.jar:${JARS}/jogl-all-mobile-natives-linux-amd64.jar:${JARS}/jogl-all-natives-linux-amd64.jar:${JARS}/jogl-all-noawt-natives-linux-amd64.jar:${JARS}/jogl-cg-natives-linux-amd64.jar:${JARS}/jogl-natives-linux-amd64.jar:${JARS}/js-1.7R2.jar:${JARS}/json-20190722.jar:${JARS}/log4j-1.2.17.jar:${JARS}/nativewindow-natives-linux-amd64.jar:${JARS}/nekohtml-1.9.22.jar:${JARS}/newt-natives-linux-amd64.jar:${JARS}/oculusvr-natives-linux-amd64.jar:${JARS}/rhino-1.7R4.jar:${JARS}/xercesImpl-2.11.0.jar:${JARS}/xml-apis-1.4.01.jar" impact.Impact3D
