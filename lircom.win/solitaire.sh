604da74cfa9da0f20385259a9da542527f499104
John Carlson <yottzumm@gmail.com>
2022-08-02 15:14:56 -0500

    changed classpath

--- lircom.win/solitaire.sh
+++ lircom.win/solitaire.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-JAVAC=/c/openjdk-17.0.1_windows-x64_bin/jdk-17.0.1/bin/javac
-JAVA=/c/openjdk-17.0.1_windows-x64_bin/jdk-17.0.1/bin/java
-JAR=/c/openjdk-17.0.1_windows-x64_bin/jdk-17.0.1/bin/jar
-KEYSTORE=mykeystore
-M2=../m2
-CLASSPATH="${M2}/xmlParserAPIs-2.6.1.jar;${M2}/nekohtml-0.9.5.jar;../target/lircom-2.jar;${M2}/httpunit.jar;${M2}/martyr.jar;${M2}/servlet-api-2.4.jar;${M2}/js-1.6R5.jar;.."
-
+export JAVAC=javac
+export JAVA=java
+export JAR=jar
+export KEYSTORE=mykeystore
+export M2=../m2
+export CLASSPATH="../target/lircom-2.jar"
 ${JAVA} -Xmx256m -classpath ${CLASSPATH} solitaire.Game dealer localhost 8180
