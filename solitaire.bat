eed5f6d5b54c9aeef9c2679040990df802006c67
John Carlson <yottzumm@gmail.com>
2022-08-02 15:16:58 -0500

    changed classpath

--- solitaire.bat
+++ solitaire.bat
@@ -3,6 +3,6 @@ set JAVA=java
 set JAR=jar
 set KEYSTORE=mykeystore
 set M2=m2
-set CLASSPATH="%M2%/xmlParserAPIs-2.6.1.jar;%M2%/nekohtml-0.9.5.jar;target/lircom-2.jar;%M2%/httpunit.jar;%M2%/martyr.jar;%M2%/servlet-api-2.4.jar;%M2%/js-1.6R5.jar;.."
+set CLASSPATH="target/lircom-2.jar"
 
 %JAVA% -Xmx256m -classpath %CLASSPATH% solitaire.Game dealer localhost 8180
