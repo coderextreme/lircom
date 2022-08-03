3461660097597697a53e86102226637799ba29a1
John Carlson <yottzumm@gmail.com>
2022-08-03 01:36:59 -0500

    scripts and Makfiles should be working

--- impact3d.sh
+++ impact3d.sh
@@ -3,11 +3,12 @@
 mvn clean install
 # make target/Impact.jar
 ant
-sleep 1
 export DIR=`pwd`
-export DWN=/C/Users/john/Downloads
+export DWN=C:/Users/john/Downloads
 export LIB=${DWN}/jogamp-all-platforms/lib/windows-amd64
+export CLASSPATH="${DIR}/target/lircom-2.jar:${DIR}/target/Impact.jar"
 
 ${DIR}/peer.sh &
+sleep 1
 
-java -Xmx256m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -Djava.library.path=${LIB} -Xmx256m -cp ${DIR}/target/Impact.jar impact.Impact3D
+java -Xmx256m -Djava.library.path=${LIB} --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -Djava.library.path=${LIB} -Xmx256m -cp ${CLASSPATH} impact.Impact3D
