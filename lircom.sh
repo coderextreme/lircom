39e82a61b8dd133cb3b08f9cc2c3454588c4a18e
John Carlson <yottzumm@gmail.com>
2022-08-02 21:28:21 -0500

    added peer to lircom

--- lircom.sh
+++ lircom.sh
@@ -1,4 +1,6 @@
 export DIR=`pwd`
 export CLASSPATH="$DIR/target/lircom-2.jar:$DIR/m2/httpunit-1.7.3.jar:$DIR/m2/nekohtml-1.9.22.jar:$DIR/m2/rhino-1.7R4.jar:$DIR/m2/json-20190722.jar"
 
+${DIR}/peer.sh &
+
 java -Xmx256m -cp $CLASSPATH lircom.MainWindow lircom.Chat
