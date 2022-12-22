0b3a083e32fd1e08581df240249e8856ddfb9407
John Carlson <yottzumm@gmail.com>
2022-12-21 18:33:34 -0600

    Updated

--- impact3d.bat
+++ impact3d.bat
@@ -4,6 +4,6 @@ set DWN=C:\Users\john\Downloads
 set M2=m2
 set LIB=C:\Users\john\Downloads\jogamp-all-platforms\lib\windows-amd64
 
-%DIR%\peer.sh
+start %DIR%\peer.bat
 
 java -Xmx256m --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -Djava.library.path=%LIB% -Xmx256m -cp %DIR%\target\Impact.jar impact.Impact3D
