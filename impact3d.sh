93af1b41face59098964f9e241fe852c27d88d49
John Carlson <yottzumm@gmail.com>
2022-12-21 19:03:16 -0600

    Updated

--- impact3d.sh
+++ impact3d.sh
@@ -1,14 +1,13 @@
-#!/bin/bash -x
+#!/bin/bash
 # make target/lircom-2.jar
-mvn clean install
+# mvn clean install
 # make target/Impact.jar
-ant
+# ant
 export DIR=`pwd`
-export DWN=C:/Users/john/Downloads
+export DWN=/c/Users/john/Downloads
 export LIB=${DWN}/jogamp-all-platforms/lib/windows-amd64
-export CLASSPATH="${DIR}/target/lircom-2.jar:${DIR}/target/Impact.jar"
 
 ${DIR}/peer.sh &
 sleep 1
 
-java -Xmx256m -Djava.library.path=${LIB} --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -Djava.library.path=${LIB} -Xmx256m -cp ${CLASSPATH} impact.Impact3D
+java -Xmx256m -Djava.library.path=${LIB} --add-exports java.base/java.lang=ALL-UNNAMED --add-exports java.desktop/sun.awt=ALL-UNNAMED --add-exports java.desktop/sun.java2d=ALL-UNNAMED -Djava.library.path=${LIB} -Xmx256m -cp "./target/Impact.jar;./target/lircom-2.jar" impact.Impact3D
