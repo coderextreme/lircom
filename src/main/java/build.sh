c7bb034afd899eb124d0ee7874aa270f65274e38
John W Carlson <yottzumm@gmail.com>
2025-07-09 00:25:30 -0500

    Upgraded htmlunit to 2.70.0 (from 2.55.0)

--- src/main/java/build.sh
+++ src/main/java/build.sh
@@ -1,2 +1,2 @@
-javac --release 17 -cp "jackson-core-2.17.1.jar;jackson-databind-2.17.1.jar;/c/Users/jcarl/lircom/m2/json-20190722.jar/c/Users/jcarl/lircom/m2/htmlunit-2.55.0.jar;/c/Users/jcarl/lircom/m2/httpclient-4.5.13.jar;/c/Users/jcarl/lircom/m2/httpunit-1.7.3.jar" {solitaire,impactVL,lircom}/*java `find f00f -name '*java'|grep -v test|grep -v Test` 2>&1
+javac --release 17 -cp "jackson-core-2.17.1.jar;jackson-databind-2.17.1.jar;/c/Users/jcarl/lircom/m2/json-20190722.jar/c/Users/jcarl/lircom/m2/htmlunit-2.70.0.jar;/c/Users/jcarl/lircom/m2/httpclient-4.5.13.jar;/c/Users/jcarl/lircom/m2/httpunit-1.7.3.jar" {solitaire,impactVL,lircom}/*java `find f00f -name '*java'|grep -v test|grep -v Test` 2>&1
 
